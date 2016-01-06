9e9271d8a40e9c63ebcb848b15ba048b7d34dab6
Christopher Weir <christopher_weir@me.com>
2016-01-06 15:24:00 +0000

    Added new methods to the compile file method

--- src/public_methods/compile-file.js
+++ src/public_methods/compile-file.js
@@ -6,8 +6,13 @@ var parsers = require('../parsers');
 // load up a file and
 module.exports = function( _file, _style ) {
 
-    var loader = loaders.fs();
-    var file = loader.load( _file );
+    var loader  = loaders.fs();
+    var file    = loader.load( _file );
+    var style   = loader.load( _style );
+    var tokens  = parsers.parseTokens( file );
 
-    parsers.parse( file );
+    var cleanDocument = parsers.cleanDocument( tokens );
+
+
+    console.log( cleanDocument );
 };
