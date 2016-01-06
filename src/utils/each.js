9b22e9ba63ea98519adc06f66779a3bb0ce89cd0
Christopher Weir <christopher_weir@me.com>
2016-01-06 14:24:58 +0000

    Added an each utility

---
+++ src/utils/each.js
@@ -0,0 +1,32 @@
+'use strict';
+var isArray = require('./is-array');
+
+/**
+ * Iterate over an array or object.
+ * @param  {array|object} obj Enumerable object.
+ * @param  {Function}     fn  Callback function executed for each item.
+ * @return {array|object}     The original input object.
+ */
+module.exports = function(obj, fn) {
+    var i, l;
+
+    if (isArray(obj)) {
+        i = 0;
+        l = obj.length;
+        for (i; i < l; i += 1) {
+            if (fn(obj[i], i, obj) === false) {
+                break;
+            }
+        }
+    } else {
+        for (i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                if (fn(obj[i], i, obj) === false) {
+                    break;
+                }
+            }
+        }
+    }
+
+    return obj;
+};
