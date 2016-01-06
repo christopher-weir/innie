457dfce963ded155bef7e1347d09a7c73bbdbe68
Christopher Weir <christopher_weir@me.com>
2016-01-06 14:25:16 +0000

    updates to the is array utility

--- src/utils/is-array.js
+++ src/utils/is-array.js
@@ -1,11 +1,7 @@
 'use strict';
 
 var isArray;
-/**
- * Test if an object is an Array.
- * @param {object} obj
- * @return {boolean}
- */
-exports.isArray = isArray = (Array.hasOwnProperty('isArray')) ? Array.isArray : function (obj) {
-  return (obj) ? (typeof obj === 'object' && Object.prototype.toString.call(obj).indexOf() !== -1) : false;
+
+module.exports = function(obj) {
+    return (obj) ? (typeof obj === 'object' && Object.prototype.toString.call(obj).indexOf() !== -1) : false;
 };
