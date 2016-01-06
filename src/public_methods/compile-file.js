00f6d3f1f7eb02bf8711ece6ed3e0f2fb35b1df6
Christopher Weir <christopher_weir@me.com>
2016-01-06 16:20:52 +0000

    Added a write file test

--- src/public_methods/compile-file.js
+++ src/public_methods/compile-file.js
@@ -3,6 +3,8 @@
 var loaders = require('../loaders');
 var parsers = require('../parsers');
 
+var fs = require('fs');
+
 // load up a file and
 module.exports = function( _file, _style ) {
 
@@ -11,8 +13,18 @@ module.exports = function( _file, _style ) {
     var style   = loader.load( _style );
     var tokens  = parsers.parseTokens( file );
 
+    tokens.tokens  = parsers.parseStyles( tokens, style );
+
     var cleanDocument = parsers.cleanDocument( tokens );
 
+    // console.log(cleanDocument);
 
-    console.log( cleanDocument );
+    // just a test
+    fs.writeFile('./index.html', cleanDocument, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log('The file was saved!');
+    });
 };
