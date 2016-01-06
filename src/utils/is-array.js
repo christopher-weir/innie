817b28c981962a25dd66ec51845fb9ee25a88c79
Christopher Weir <christopher_weir@me.com>
2016-01-06 12:50:03 +0000

    added is array middleware

---
+++ src/utils/is-array.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var isArray;
+/**
+ * Test if an object is an Array.
+ * @param {object} obj
+ * @return {boolean}
+ */
+exports.isArray = isArray = (Array.hasOwnProperty('isArray')) ? Array.isArray : function (obj) {
+  return (obj) ? (typeof obj === 'object' && Object.prototype.toString.call(obj).indexOf() !== -1) : false;
+};
