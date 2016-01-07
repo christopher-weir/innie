b8025c5e1b5134912868991fed245ccce687e436
Christopher Weir <christopher_weir@me.com>
2016-01-07 14:11:21 +0000

    Added validate options method

---
+++ src/utils/validate-options.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Validate the options object.
+ * @return {undefined}      This method will throw errors if anything is wrong.
+ */
+module.exports = function( _options ) {
+    if ( !_options ) {
+        return;
+    }
+};
