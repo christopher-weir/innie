421bdf0da14278a81ba23c0c2f237827137dd838
Christopher Weir <christopher_weir@me.com>
2016-01-08 12:24:19 +0000

    Added get style properties method to the utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 exports.extend          = require('./extend');
 exports.escapeRegExp    = require('./escape-reg-exp');
+exports.getStyleProperties    = require('./get-style-properties');
 exports.elementRegExp   = require('./element-reg-exp');
 exports.each            = require('./each');
 exports.validateOptions            = require('./validate-options');
