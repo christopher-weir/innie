a19d9e9e4dc82d4baaa86c33e1758d14ef7580ee
Christopher Weir <christopher_weir@me.com>
2016-01-11 11:12:11 +0000

    refactor createNode method

--- src/parsers/parse-html.js
+++ src/parsers/parse-html.js
@@ -16,10 +16,30 @@ module.exports = function( _tokens ) {
         return style;
     };
 
+    // TODO refactor
+    // extend to utils
     var createNode = function( _token ){
+
+        var elementStart = _token.original.split( utils.elementRegExp( '<',' ' ) );
+        console.log(elementStart[ 1 ]);
+
         var nodeArray = _token.original.split( utils.elementRegExp( 'class="','"' ) );
 
+        var classAttr = elementStart[ 2 ].split( utils.elementRegExp( 'class="','"' ) );
+
+        var styleAttr = '';
+
+        if( _token.style ){
+            styleAttr = classAttr[ 2 ].split( _token.styleAttr );
+        }
+
+        var elementEnd = '';
+
         var style = addStyle( _token.styleToAdd );
+
+        var newNode = String( elementStart[ 1 ] + _token.classAttr + style ).replace(/\s\s+/g, ' ');
+
+        console.log(newNode);
 
         return String( nodeArray[ 0 ] + _token.classAttr + style + nodeArray[ 2 ] ).replace(/\s\s+/g, ' ');
     };
