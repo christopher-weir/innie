f25725317c33812ddb3ff8119a8ebbfec5f49e85
Christopher Weir <christopher_weir@me.com>
2016-01-11 17:47:47 +0000

    Added markup utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,10 @@
 exports.extend                      = require('./extend');
 exports.escapeRegExp                = require('./escape-reg-exp');
-exports.getStyleProperties          = require('./get-style-properties');
 exports.elementRegExp               = require('./element-reg-exp');
 exports.styleRegExp                 = require('./style-reg-exp');
 exports.each                        = require('./each');
 exports.validateOptions             = require('./validate-options');
 exports.createElement               = require('./create-element');
+
+exports.getArrayOfClasses           = require('./markup/get-array-of-classes');
+exports.getStyleProperties          = require('./markup/get-style-properties');
