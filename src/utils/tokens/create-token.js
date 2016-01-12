f2c284c3a2a25431b1c48852aa0a8ddce3687835
Christopher Weir <christopher_weir@me.com>
2016-01-12 11:27:47 +0000

    Added create token util

---
+++ src/utils/tokens/create-token.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function( _chunk, _index ) {
+    // console.log( _index );
+    // console.log( _chunk );
+
+    var token = {
+        original    : _chunk,
+        matches     : []
+    };
+
+    return token;
+};
