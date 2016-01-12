88dd10fcdef06ea2fcb22db07a882c42d4e2ea50
Christopher Weir <christopher_weir@me.com>
2016-01-12 13:18:08 +0000

    Updates to the create token util

--- src/utils/tokens/create-token.js
+++ src/utils/tokens/create-token.js
@@ -1,13 +1,48 @@
 'use strict';
 
-module.exports = function( _chunk, _index ) {
-    // console.log( _index );
-    // console.log( _chunk );
+var getArrayOfClasses           = require('../markup/get-array-of-classes');
+var getStyleProperties           = require('../markup/get-style-properties');
+var classRegExp                 = require('../class-reg-exp');
+var styleRegExp                 = require('../style-reg-exp');
+
+
+module.exports = function( _chunk, _options, _index ) {
+
+    var i = 0;
+    var style = '';
+    var hasStyle = false;
+    var styleProperties = [];
+
+    if( _chunk.split( styleRegExp() )[1] ){
+        hasStyle = true;
+        style = _chunk.split( styleRegExp() )[1];
+        styleProperties = getStyleProperties( style );
+    }
 
     var token = {
+        index       : _index || 0,
         original    : _chunk,
-        matches     : []
+        style       : {
+            has_style   : hasStyle,
+            original    : style,
+            properties  : styleProperties
+        },
+        class       : {
+            original    : _chunk.split( classRegExp() )[1],
+            all         : getArrayOfClasses( _chunk.split( classRegExp() )[1] ),
+            matches     : [],
+            unmatched   : []
+        }
     };
+
+
+    for ( i = 0; i < token.class.all.length; i++) {
+        if( token.class.all[ i ].includes( _options.hook ) ){
+            token.class.matches.push( token.class.all[ i ] );
+        }else{
+            token.class.unmatched.push( token.class.all[ i ] );
+        }
+    }
 
     return token;
 };
