28f13a004607c57ddd17d97d9a5c44fecafcf78f
Christopher Weir <christopher_weir@me.com>
2016-01-12 15:42:13 +0000

    Added css utils

---
+++ src/utils/css/get-class-props.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var cssClassRegExp          = require('../css-class-reg-exp');
+var elementRegExp           = require('../element-reg-exp');
+
+
+module.exports = function( _class ) {
+
+
+    var classProps      = [];
+    var styleToAdd      = _class.split( elementRegExp( '{','}' ) );
+
+    classProps = styleToAdd[ 1 ].replace(/(\r\n|\n|\r|[{}])/gm,'').split(';');
+    console.log(classProps);
+    return classProps;
+};
---
+++ src/utils/css/get-matching-class.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var cssClassRegExp          = require('../css-class-reg-exp');
+
+
+module.exports = function( _name, _src, _options ) {
+
+    // utils.cssClassRegExp(  )
+    var classAttr   = false;
+    var name        = _name.replace( _options.hook, '');
+    var splitArray  = _src.split( cssClassRegExp( name ) );
+
+    if( splitArray[ 1 ] ){
+        classAttr = splitArray[ 1 ];
+    }
+
+    return classAttr;
+};
