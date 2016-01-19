1dc91b0c2f647f95bedf1a63306580ffae53878d
Christopher Weir <christopher_weir@me.com>
2016-01-19 12:55:26 +0000

    Moved ergexp to new folder

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,11 @@
 exports.extend                      = require('./extend');
-exports.escapeRegExp                = require('./escape-reg-exp');
-exports.elementRegExp               = require('./element-reg-exp');
-exports.styleRegExp                 = require('./style-reg-exp');
-exports.cssClassRegExp              = require('./css-class-reg-exp');
 exports.each                        = require('./each');
 exports.validateOptions             = require('./validate-options');
+
+exports.escapeRegExp                = require('./reg-exp/escape-reg-exp');
+exports.elementRegExp               = require('./reg-exp/element-reg-exp');
+exports.styleRegExp                 = require('./reg-exp/style-reg-exp');
+exports.cssClassRegExp              = require('./reg-exp/css-class-reg-exp');
 
 exports.getArrayOfClasses           = require('./markup/get-array-of-classes');
 exports.getStyleProperties          = require('./markup/get-style-properties');
