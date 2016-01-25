90f140036ce177422d38db0d31e7f1d40a6d5fcc
Christopher Weir <christopher_weir@me.com>
2016-01-25 12:49:27 +0000

    Added new create token

---
+++ src/utils/tokens/create-react-token.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var getArrayOfClasses           = require('../markup/get-array-of-classes');
+var getStyleProperties          = require('../markup/get-style-properties');
+var classRegExp                 = require('../reg-exp/class-reg-exp');
+var reactStyleRegExp            = require('../reg-exp/react-style-reg-exp');
+
+
+module.exports = function( _chunk, _options, _index ) {
+
+    var i = 0;
+    var style = '';
+    var hasStyle = false;
+    var styleProperties = [];
+
+    if( _chunk.split( reactStyleRegExp() )[1] ){
+        hasStyle = true;
+        style = _chunk.split( reactStyleRegExp() )[1];
+        // styleProperties = getStyleProperties( style );
+    }
+
+    var token = {
+        // index       : _index || 0,
+        // original    : _chunk,
+        style       : {
+            has_style   : hasStyle,
+            original    : style,
+            properties  : styleProperties,
+            compiled    : ''
+        },
+        // class       : {
+        //     original    : _chunk.split( classRegExp() )[1],
+        //     all         : getArrayOfClasses( _chunk.split( classRegExp() )[1] ),
+        //     matches     : [],
+        //     unmatched   : []
+        // }
+    };
+
+
+    // for ( i = 0; i < token.class.all.length; i++) {
+    //     if( token.class.all[ i ].includes( _options.hook ) ){
+    //         token.class.matches.push( token.class.all[ i ] );
+    //     }else{
+    //         token.class.unmatched.push( token.class.all[ i ] );
+    //     }
+    // }
+
+    return token;
+};
