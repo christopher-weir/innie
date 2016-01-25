b44e83ff2db8c0e476389781995a611a81770a7f
Christopher Weir <christopher_weir@me.com>
2016-01-25 12:50:45 +0000

    Updates to the react test

--- test/test-parsers/test-parse-react.js
+++ test/test-parsers/test-parse-react.js
@@ -6,14 +6,42 @@ var parsers     = require('../../src/parsers');
 var expect      = require('chai').expect;
 
 
-describe('Parse react file', function(){
+var testReactFile = 'var Timer = React.createClass({displayName: "Timer",getInitialState: function getInitialState() {return { secondsElapsed: 0 };},tick: function tick() {this.setState({ secondsElapsed: this.state.secondsElapsed + 1 });},componentDidMount: function componentDidMount() {this.interval = setInterval(this.tick, 1000);},componentWillUnmount: function componentWillUnmount() {clearInterval(this.interval);},render: function render() {return React.createElement("div",{ className: "test agagin #new", style: { "color": "red" } },"Seconds Elapsed:",this.state.secondsElapsed);}});';
+var splitSrc = utils.splitReact( testReactFile );
+var chunk = 'React.createElement("div",{ className: "test agagin #new", style: { "color": "red" } },"Seconds Elapsed: ",this.state.secondsElapsed);';
 
-    var testReactFile = 'var Timer = React.createClass({displayName: "Timer",getInitialState: function getInitialState() {return { secondsElapsed: 0 };},tick: function tick() {this.setState({ secondsElapsed: this.state.secondsElapsed + 1 });},componentDidMount: function componentDidMount() {this.interval = setInterval(this.tick, 1000);},componentWillUnmount: function componentWillUnmount() {clearInterval(this.interval);},render: function render() {return React.createElement("div",{ className: "test agagin #new", style: { "color": "red" } },"Seconds Elapsed:",this.state.secondsElapsed);}});';
 
-    it('Should split the file at createElement', function(){
-console.log(utils.splitReact( testReactFile )[1]);
-        expect( utils.splitReact( testReactFile )  )
+describe('Parse React file', function(){
+
+
+    it('Should split the file at React.createElement', function(){
+        expect( splitSrc )
             .to.be.an('array');
     });
+
+});
+
+
+describe('React Token', function(){
+
+    it('Token should be an object', function(){
+        expect( utils.createReactToken( chunk )  )
+            .to.be.an('object');
+    });
+
+    it('Token should have a style', function(){
+        expect( utils.createReactToken( chunk )  )
+            .to.have.deep.property('style.has_style')
+            .to.be.a('boolean')
+            .to.eql(true);
+    });
+
+    it('Token style should be a string and matching', function(){
+        expect( utils.createReactToken( chunk )  )
+            .to.have.deep.property('style.original')
+            .to.be.a('string')
+            .to.eql('style: { "color": "red" }');
+    });
+
 
 });
