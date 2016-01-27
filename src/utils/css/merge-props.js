ea305079257c47cfeb47af44f2b36a4d7c826287
Christopher Weir <christopher_weir@me.com>
2016-01-27 12:00:17 +0000

    Refactored function and split for testing

--- src/utils/css/merge-props.js
+++ src/utils/css/merge-props.js
@@ -1,34 +1,15 @@
 'use strict';
+var mergeCompiledProps  = require('./merge-compiled-props');
+var createCleanProps    = require('./create-clean-props');
 
 
 module.exports = function( _newProps, _oldProps, _options ) {
 
-    var i = 0;
     var oldProps    = _oldProps || []; // props from the style attr
     var classProps  = oldProps.concat( _newProps );
     var options     = _options;
 
-    var cleanProps = {};
-    var params = '';
+    var params = mergeCompiledProps( createCleanProps( classProps ), options );
 
-    for ( i = 0; i < classProps.length; i++ ) {
-
-        if( classProps[ i ] && classProps[ i ].includes(':') ){
-            var key = classProps[ i ].split(':')[ 0 ].replace(/\s\s+/g, ' ');
-            cleanProps[ key ] = {};
-            cleanProps[ key ] = classProps[ i ].split(':')[ 1 ];
-        }
-    }
-
-    for (var _key in cleanProps) {
-
-        switch ( options.type ) {
-            case 'react':
-                params = params + '\'' + _key + '\':' + '\'' + cleanProps[ _key ].trim() + '\',';
-                break;
-            default:
-                params = params + _key + ':' + cleanProps[ _key ] + '; ';
-        }
-    }
-    return params.trim();
+    return params;
 };
