920e06d590f699b4e1983f872a60a9585cfb7fc0
Christopher Weir <christopher_weir@me.com>
2016-02-24 14:28:49 +0000

    added test files

--- test/test-utils/test-patterns/markup.js
+++ test/test-utils/test-patterns/markup.js
@@ -45,3 +45,24 @@ exports.reactNodeToken = function() {
 exports.reactNodeTokenExpected = function() {
     return '{ className: \'test agagin\',   style: {\'color\':\'purple\',\'font-size\':\'26px\',\'margin-bottom\':\'1em !important\',\'background-color\':\'green\'}';
 };
+
+exports.reactFile = function(){
+    return '\'use strict\';var React = \'\';var Timer = React.createClass({  displayName: \'Timer\',  getInitialState: function getInitialState() {    return { secondsElapsed: 0 };  },  tick: function tick() {    this.setState({ secondsElapsed: this.state.secondsElapsed + 1 });  },  componentDidMount: function componentDidMount() {    this.interval = setInterval(this.tick, 1000);  },  componentWillUnmount: function componentWillUnmount() {    clearInterval(this.interval);  },  render: function render() {    return React.createElement(      \'div\',      { className: \'test agagin #new\', style: { \'color\': \'red\' } },      \'Seconds Elapsed: \',      this.state.secondsElapsed,      React.createElement(        \'p\',        { className: \'test agagin #awefweaf\', style: { \'color\': \'red\' } },        \'this isa content\',        React.createElement(          \'p\',           { className: \'test agagin #new\',  style: {\'color\':\'red\',\'font-size\':\'26px\',\'margin-bottom\':\'1em !important\'}  },          \'this isa content\'        )      )    );  }});';
+};
+exports.reactSplitFileExpected = function(){
+return [ '\'use strict\';var React = \'\';var Timer = React.createClass(',
+  '{  displayName: \'Timer\',  getInitialState: function getInitialState() {    return { secondsElapsed: 0 }',
+  ';  },  tick: function tick() ',
+  '{    this.setState({ secondsElapsed: this.state.secondsElapsed + 1 }',
+  ');  },  componentDidMount: function componentDidMount() ',
+  '{    this.interval = setInterval(this.tick, 1000);  }',
+  ',  componentWillUnmount: function componentWillUnmount() ',
+  '{    clearInterval(this.interval);  }',
+  ',  render: function render() ',
+  '{    return React.createElement(      \'div\',      { className: \'test agagin #new\', style: { \'color\': \'red\' }',
+  ' },      \'Seconds Elapsed: \',      this.state.secondsElapsed,      React.createElement(        \'p\',        ',
+  '{ className: \'test agagin #awefweaf\', style: { \'color\': \'red\' }',
+  ' },        \'this isa content\',        React.createElement(          \'p\',           ',
+  '{ className: \'test agagin #new\',  style: {\'color\':\'red\',\'font-size\':\'26px\',\'margin-bottom\':\'1em !important\'}',
+  '  },          \'this isa content\'        )      )    );  }});' ];
+};
