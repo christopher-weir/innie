d6706b7ffbfa75d1d452f83a200068ab2b611a5f
Christopher Weir <christopher_weir@me.com>
2016-02-29 15:52:09 +0000

    Added new comments

--- src/utils/css/create-clean-props.js
+++ src/utils/css/create-clean-props.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * createCleanProps
+ * Converts an array of css properties to an object
+ * This makes it easier to merge later
+ * @method function
+ * @param  {Array} classProps   array of the props ot use
+ * @return {Object}             Object of the props
+ */
 module.exports = function( classProps ){
     var cleanProps = {};
     var i = 0;
--- src/utils/css/merge-compiled-props.js
+++ src/utils/css/merge-compiled-props.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * mergeCompiledProps
+ * Convert the object to a css string for the required options type
+ * @method function
+ * @param  {Object} cleanProps Cleaned props object
+ * @param  {Object} options    The innie options
+ * @return {string}            The compiled css string
+ */
 module.exports = function( cleanProps, options ){
 
     var params = '';
@@ -14,10 +22,11 @@ module.exports = function( cleanProps, options ){
                 params = params + _key + ':' + cleanProps[ _key ] + '; ';
         }
     }
+
     // clean out any whitespace on the ends
     params = params.trim();
 
-    // remove the last , if it exists
+    // remove the last ',' if it exists
     if( params.slice( -1 ) === ',' ){
         params = params.slice( 0, -1 );
     }
