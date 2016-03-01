8df96a90758f5c85aea49ee78e7d1e7d244cce4f
Christopher Weir <christopher_weir@me.com>
2016-03-01 11:40:52 +0000

    Cleaned up the test doc added new test for get-class-props

--- test/test-utils/css.js
+++ test/test-utils/css.js
@@ -5,21 +5,35 @@ var utils = require('../../src/utils');
 
 var expect = require('chai').expect;
 
+var testPropsArray = ['color: red', 'font-size: 26px', 'margin-bottom: 1em !important', ''];
+var testPropsObj = {
+    'color': ' purple',
+    'font-size': '26px',
+    'margin-bottom': '1em !important',
+    'background-color': ' green'
+};
+var options = {
+    hook: '#',
+    location: './',
+    file_name: null,
+    file: 'test/react.js',
+    type: 'react',
+    style: 'test/style.css'
+};
+var testClass = '.test-class {opacity: 0.5;pointer-events: none;}';
 
 describe('Create Clean Props: create-clean-props.js', function() {
 
-    var testProps = ['color: red', 'font-size: 26px', 'margin-bottom: 1em !important', ''];
-
     it('Should return an object', function() {
 
-        expect(utils.css.createCleanProps(testProps))
+        expect(utils.css.createCleanProps(testPropsArray))
             .to.be.an('object');
 
     });
 
     it('Should be matching', function() {
 
-        expect(utils.css.createCleanProps(testProps))
+        expect(utils.css.createCleanProps(testPropsArray))
             .to.have.all.keys('color', 'font-size', 'margin-bottom');
 
     });
@@ -29,27 +43,22 @@ describe('Create Clean Props: create-clean-props.js', function() {
 
 describe('Merge Compiled Props: merge-compiled-props.js', function() {
 
-    var testProps = {
-        'color': ' purple',
-        'font-size': '26px',
-        'margin-bottom': '1em !important',
-        'background-color': ' green'
-    };
-
-    var options = {
-        hook: '#',
-        location: './',
-        file_name: null,
-        file: 'test/react.js',
-        type: 'react',
-        style: 'test/style.css'
-    };
-
     it('Should return a string and be eql', function() {
 
-        expect(utils.css.mergeCompiledProps( testProps, options ))
+        expect(utils.css.mergeCompiledProps(testPropsObj, options))
             .to.be.a('string')
             .to.eql('\'color\':\'purple\',\'font-size\':\'26px\',\'margin-bottom\':\'1em !important\',\'background-color\':\'green\'');
+    });
+
+});
+
+
+describe('Get Class Props: get-class-props.js', function() {
+
+    it('Should return an array', function() {
+
+        expect(utils.css.getClassProps(testClass))
+            .to.be.an('array');
     });
 
 });
